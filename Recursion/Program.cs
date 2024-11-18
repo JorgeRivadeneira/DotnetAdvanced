b7c818b36b1f570e892affa97d46542e60c3f2dd
Jorge Rivadeneira Cevallos <jorge.rivadeneira.cevallos@gmail.com>
2024-11-18 09:58:52 -0500

    Adding recursion

---
+++ Recursion/Program.cs
@@ -0,0 +1,20 @@
+﻿const string directoryPath = @"C:\flutter";
+
+DisplayDirectoriesInDirectory(directoryPath, 1);
+
+
+
+static void DisplayDirectoriesInDirectory(string path, int depth = 0)
+{
+
+    string[] directories = Directory.GetDirectories(path);
+
+    foreach (string directory in directories)
+    {
+        Console.WriteLine(directory);
+        if(depth > 0)
+        {
+            DisplayDirectoriesInDirectory(directory, depth - 1);
+        }
+    }
+}
